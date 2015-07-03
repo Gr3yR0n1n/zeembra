4eb8821e61eda2ea29a1a6272ccc34cd7b5573f7
davis <lawrence.davis@gmail.com>
2015-07-03 13:14:25 -0600

    Removed default COS restriction

--- zcs-cos-export.sh
+++ zcs-cos-export.sh
@@ -20,14 +20,10 @@ cosList=`su - zimbra -c 'zmprov gac'`;
 arr=$(echo $cosList | tr " " "\n");
 for cosName in $arr
 do
-	if [ $cosName = "default" ] || [ $cosName = "defaultExternal" ]; then
-		echo "Skipping system cos, $cosName..."
-	else
-		su - zimbra -c 'echo '$cosName' >> '$cosFolder'/cos.lst'
-		echo "Exporting $cosName"
-		su - zimbra -c 'zmprov -l gc '$cosName' > '$cosFolder'/'$cosName'.tmp'
-		cat $cosFolder/$cosName.tmp | sed 's/^/mc '$cosName' /g' | sed 's/: / /g' > $cosFolder/$cosName.cos
-	fi
+	su - zimbra -c 'echo '$cosName' >> '$cosFolder'/cos.lst'
+	echo "Exporting $cosName"
+	su - zimbra -c 'zmprov -l gc '$cosName' > '$cosFolder'/'$cosName'.tmp'
+	cat $cosFolder/$cosName.tmp | sed 's/^/mc '$cosName' /g' | sed 's/: / /g' > $cosFolder/$cosName.cos
 done
 
 rm $cosFolder/*.tmp
